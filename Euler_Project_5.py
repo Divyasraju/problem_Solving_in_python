9876f0ac70b528ea3d397e99f4aebab9e84fb900
Divya <divyasraju93@gmail.com>
2018-07-29 13:02:08 +0530

    Solution format for problem 5

---
+++ Euler_Project_5.py
@@ -0,0 +1,16 @@
+'''2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?'''
+
+# returns smallest multiple that is evenly divisible by all numbers from 1 - n
+# returns -1 if multiple does not exist
+def factorial(x):
+    pass
+
+def gcd(a, b):
+    pass
+
+def lcm(*k):
+    pass
+
+lcm(list(range(1, 21)))
